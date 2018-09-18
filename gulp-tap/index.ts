ba3ae55a9c1c9611ab0431675a6f6aef76d0c474
l5x <189198+l5x@users.noreply.github.com>
2018-09-18 21:37:04 +0000

    fix: gulp-tap/.ts/.d.ts/

--- gulp-tap/index.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-
-import File = require('vinyl')
-
-// tslint:disable-next-line no-any
-declare function tap(lambda: (file: File, t: any) => void): NodeJS.ReadWriteStream
-
-export = tap
