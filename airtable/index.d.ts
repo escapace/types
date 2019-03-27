e57b7cd12086d08a8400ac639432c0f5c639a419
l5x <189198+l5x@users.noreply.github.com>
2019-03-27 12:58:11 +0400

    feat: airtable types

--- airtable/index.d.ts
+++ airtable/index.d.ts
@@ -9,9 +9,69 @@ declare namespace Airtable {
     noRetryIfRateLimited: boolean
   }
 
-  function configure (options: Partial<Airtable.Options>): void
+  interface FieldSet {
+    [key: string]: undefined | string | ReadonlyArray<Attachment>
+  }
 
-  function base (id: string): any
+  // type Base<T extends FieldSet> = (tableName: string) => Table<T>
+
+  interface Base {
+    <T extends FieldSet> (tableName: string): Table<T>
+  }
+
+  interface Table<TFields extends FieldSet> {
+    select(opt?: SelectOptions): Query<TFields>
+    find(id: string): Promise<Response<TFields>>
+    create(record: Partial<TFields>): Promise<Response<TFields>>
+    update(id: string, record: Partial<TFields>): Promise<Response<TFields>>
+    replace(id: string, record: Partial<TFields>): Promise<Response<TFields>>
+    destroy(id: string): Promise<{ id: string}>
+  }
+
+  interface SelectOptions {
+    view?: string
+    maxRecords?: number
+  }
+
+  interface Query<TFields extends object> {
+    all(): Promise<Response<TFields>>
+    firstPage(): Promise<Response<TFields>>
+    eachPage(cb: (records: Response<TFields>, fetchNextPage: () => void) => void, done: (err: unknown) => void): void
+  }
+
+  type Response<TFields> = ReadonlyArray<Row<TFields>>
+
+  interface Row<TFields> {
+    id: string
+    fields: TFields
+  }
+
+  interface Attachment {
+    id: string
+    url: string
+    filename: string
+    size: number
+    type: string
+    thumbnails?: {
+      small: Thumbnail
+      large: Thumbnail
+      full: Thumbnail
+    }
+  }
+
+  interface Thumbnail {
+    url: string
+    width: number
+    height: number
+  }
+
+  interface Table<TFields extends FieldSet> {
+    select(opt?: SelectOptions): Query<TFields>
+  }
+
+  function configure(options: Partial<Airtable.Options>): void
+
+  function base(talbeName: string): Base
 }
 
 export = Airtable
