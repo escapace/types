57613a92bb13a4af67cc8c2ecd95dd6808fe66e2
l5x <189198+l5x@users.noreply.github.com>
2019-12-12 08:15:48 +0400

    feat: add lerp-array

---
+++ lerp-array/index.d.ts
@@ -0,0 +1,12 @@
+declare function lerpArray(
+  value1: number[],
+  value2: number[],
+  t: number
+): number[]
+declare function lerpArray(
+  value1: number,
+  value2: number,
+  t: number
+): number
+
+export = lerpArray
